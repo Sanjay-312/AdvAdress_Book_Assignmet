899239ed0ec9d7aae546e3377a114f6d6d22aaf7
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-26 08:26:19 +0530

    [sanjeeva] : created database

---
+++ AdressBookOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOassignmnet
+{
+    internal class AdressBookOperations
+    {
+        public static void create_database()
+        {
+           
+           SqlConnection connection = new SqlConnection("data source=(localDB)\\MSSQLLocalDB; initial catalog=master; integrated security=true");
+            string query = "create database Address_Book_Service";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("db creted successfully");
+            Console.WriteLine("-------------------------");
+            connection.Close();
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADOassignmnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdressBookOperations.create_database();
+        }
+    }
+}
